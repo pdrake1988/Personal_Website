47fe63dc76d63343fc5e452e0a6507e6665737bf
pdrake1988 <pdrake1@me.com>
2021-06-27 20:53:23 -0500

    Created Movie Project

---
+++ Personal_Website/Controllers/MovieProjectController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Personal_Website.Controllers
+{
+    public class MovieProjectController : Controller
+    {
+        // GET
+        public IActionResult GetMovies()
+        {
+            return View();
+        }
+    }
+}
---
+++ Personal_Website/Models/Movie Project/Root.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Personal_Website.Models.Movie_Project
+{
+    public class Root : MovieResults
+    {
+        [JsonPropertyName("page")]
+        public int Page { get; set; }
+
+        [JsonPropertyName("results")]
+        public List<MovieResults> Results { get; set; }
+
+        [JsonPropertyName("total_pages")]
+        public int TotalPages { get; set; }
+
+        [JsonPropertyName("total_results")]
+        public int TotalResults { get; set; }
+    }
+}
